3d97ef57e73b0e3223fbe5987cbe3e49cfec098a
aliber <aliber>
2021-03-08 23:20:00 +0800

    字节码项目初始化

---
+++ leb/javaagent/src/main/java/com/ali/leb/javaagent/JavaagentApplication.java
@@ -0,0 +1,13 @@
+package com.ali.leb.javaagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaagentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaagentApplication.class, args);
+    }
+
+}
